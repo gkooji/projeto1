005fa768fd4abe59c6dad5b592eb4c3ac948e5ee
Kooji <guikooji11@hotmail.com>
2026-08-13 11:58:09 -0300

    feat: continuação do projeto

--- src/components/Tarefas.jsx
+++ src/components/Tarefas.jsx
@@ -1,11 +1,74 @@
-
+import { useState, useEffect } from "react"
 
 const Tarefas = () => {
-  return (
-    <>
-    Tarefas
-    </>
-  )
+
+    // HOOK- useState= Manipula o estado da variável
+    const [tarefas, setTarefas] = useState(() => {
+        const salvarDados = localStorage.getItem("items-tarefas");
+        return salvarDados ? JSON.parse(salvarDados) : [];
+    });
+    const [campo, setCampo] = useState('');
+
+    //HOOK-useEffect - realiza um efeito colateral , no exmeplo vai 
+    //atualizar em tempo real a tarefa criada
+
+    useEffect(() => {
+        localStorage.setItem("items-tarefas", JSON.stringify(tarefas));
+        [tarefas]
+    })
+
+    // Arrow function adicionar campo
+    const AdicionarTarefa = (e) => {
+        e.preventDefault();
+        if (!campo.trim()) return;
+
+        const novaTarefa = {
+            id: Date.now(),
+            text: campo,
+        };
+
+        setTarefas([...tarefas, novaTarefa]);
+        setCampo('')
+    }
+
+    const RemoverTarefa = (id) => {
+        const alterarTarefa = tarefas.filter((tarefa) => { tarefa.id !== id })
+        setTarefas(alterarTarefa);
+    }
+
+    return (
+        <>
+            <div className="todo-container">
+                <h1>Minha lista de Tarefas</h1>
+                <form onSubmit={AdicionarTarefa} className="todo-form">
+                    <input
+                        type="text"
+                        value={campo}
+                        placeholder="Digite uma nova tarefa..."
+                        className="todo-input"
+                        onChange={(e) => setCampo(e.target.value)}
+                    />
+                    <button type="submit" className="btn-add">
+                        Adicionar
+                    </button>
+
+                </form>
+                <ul className="todo-list">
+                    {tarefas.map((item) => (
+                        <li key={item.id} className="todo-item">
+                            <span>{item.text}</span>
+                            <button onClick={RemoverTarefa(item.id)} className="btn-delete">
+                                
+                                Excluir
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+                {tarefas.length === 0 && <p className="todo-vazio">Nenhuma Tarefa</p>}
+            </div>
+
+        </>
+    )
 }
 
 export default Tarefas
